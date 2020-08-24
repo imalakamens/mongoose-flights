b25c335a4702de704b5b255fe0f216766c366340
Kamali Means <darenkmeans@gmail.com>
2020-08-24 15:08:41 -0700

    init repo/first commit

---
+++ controllers/flights.js
@@ -0,0 +1,37 @@
+const Flight = require('../models/flight');
+
+module.exports = {
+    index,
+    new: newFlight,
+    create,
+    show
+};
+
+function index(req, res) {
+    Flight.find({}, (err, flights) => {
+        res.render('flights/index', { flights });
+})
+}
+
+function newFlight(req, res) {
+    res.render('flights/new')
+};
+
+function create(req, res) {
+    let defaultDate = new Date(new Date().setFullYear(new Date().getFullYear() + 1)); 
+    req.body.airport == '' ? req.body.airport = 'DEN' : req.body.airport = req.body.airport;
+    req.body.departs == '' ? req.body.departs = defaultDate : req.body.departs = req.body.departs;
+    const flight = new Flight(req.body);
+    flight.save( err => {
+        if(err) return res.redirect('/flights/new');
+        res.redirect('/flights');
+    });
+    console.log(req.body)
+};
+
+function show(req, res) {
+    Flight.findById(req.params.id, (err, flight) => {
+        res.render('flights/show', { title: 'Flight Detail', flight })
+    });
+    
+};
---
+++ routes/flights.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+const flightsCtrl = require('../controllers/flights.js')
+
+/* GET listing. */
+router.get('/', flightsCtrl.index);
+// GET /flights/new
+router.get('/new', flightsCtrl.new);
+// GET /flights show
+router.get('/:id', flightsCtrl.show); 
+// POST /flights
+router.post('/', flightsCtrl.create);
+
+module.exports = router;
