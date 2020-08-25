c9d34d861105e6e477e4cbc5ca136af4d8fa494a
Kamali Means <darenkmeans@gmail.com>
2020-08-24 17:05:31 -0700

    update ticket Schema

--- models/ticket.js
+++ models/ticket.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ticketSchema = new Schema( {
-    seat: { type: String },
+    seat: { type: String, match: /[A-F][1-9]\d?/ },
     price: { type: Number },
     flight: [{
     type: Schema.Types.ObjectId,
